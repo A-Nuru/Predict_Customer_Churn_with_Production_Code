bd3a716df4e702cc2a01d718bed6e7eccc6279d7
Nurudeen Adesina <access2abey@hotmail.com>
2022-08-24 02:02:45 +0100

    styling to pep8 standard

--- churn_library.py
+++ churn_library.py
@@ -6,12 +6,12 @@ Date : 23th August 2022
 
 # import libraries
 import os
-#import shap
+import shap
 import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
+import seaborn as sns
 from sklearn.preprocessing import normalize
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -27,55 +27,48 @@ def import_data(pth):
     input:
             pth: a path to the csv file
     output:
-            df: pandas dataframe
-    '''	
+            dataframe: pandas dataframe
+    '''
     return pd.read_csv(pth)
 
-def perform_eda(df):
+def perform_eda(dataframe):
     '''
     perform eda on df and save figures to images folder
     input:
             df: pandas dataframe
-
     output:
             None
     '''
-    df.drop('Unnamed: 0', axis=1, inplace=True)
-    #print(df.describe())
-
+    dataframe.drop('Unnamed: 0', axis=1, inplace=True)
+    
     # Encoding the label column
-    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
-
+    dataframe['Churn'] = dataframe['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
     # plotting and saving figures
     # Churn Distribution
     plt.figure(figsize=(20, 10))
-    df['Churn'].hist()
+    dataframe['Churn'].hist()
     plt.savefig(fname='./images/eda/churn_distribution.png')
 
     # Customer Age Distribution
     plt.figure(figsize=(20, 10))
-    df['Customer_Age'].hist()
+    dataframe['Customer_Age'].hist()
     plt.savefig(fname='./images/eda/customer_age_distribution.png')
 
     # Marital Status Distribution
     plt.figure(figsize=(20, 10))
-    df.Marital_Status.value_counts('normalize').plot(kind='bar')
+    dataframe.Marital_Status.value_counts('normalize').plot(kind='bar')
     plt.savefig(fname='./images/eda/marital_status_distribution.png')
 
     # Total Transaction Distribution
     plt.figure(figsize=(20, 10))
-    #sns.histplot(df['Total_Trans_Ct'],kde=True);
-    #plt.savefig(fname='./images/eda/total_transaction_distribution.png')
+    sns.histplot(dataframe['Total_Trans_Ct'],kde=True)
+    plt.savefig(fname='./images/eda/total_transaction_distribution.png')
 
     # Heatmap
     plt.figure(figsize=(20, 10))
-    sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths=2)
+    sns.heatmap(dataframe.corr(), annot=False, cmap='Dark2_r', linewidths=2)
     plt.savefig(fname='./images/eda/heatmap.png')
-
     
 if __name__ == '__main__':
-    # Import data
-    DF = import_data(pth='./data/bank2_data.csv')
-
-    # Perform EDA 
+    DF = import_data(pth='./data/bank_data.csv')
     perform_eda(DF)    
